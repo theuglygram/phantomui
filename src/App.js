6071adb5935208beffd60c5c76745067f2f86d91
theuglygram <iamerheren@gmail.com>
2022-04-23 01:47:26 +0100

    scroll restore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Blog from "components/pages/blog/Blog";
 import Home from "components/pages/home/Home";
 import Security from "components/pages/security/Security";
+import ScrollToTop from "components/ScrollToTop";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/security" element={<Security />} />
